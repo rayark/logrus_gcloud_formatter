d850eaf5eaf5d501802d81be42222fd3154967b4
Yao Wei <mwei@rayark.com>
2016-04-27 18:19:01 +0800

    feat: init commit

---
+++ logrus_gcloud_formatter.go
@@ -0,0 +1,53 @@
+package logrus_gcloud_formatter
+
+import (
+	"encoding/json"
+	"fmt"
+	"time"
+
+	"github.com/Sirupsen/logrus"
+)
+
+type LogrusGoogleCloudFormatter struct {
+	Type string // if not empty use for logstash type field.
+
+	// TimestampFormat sets the format used for timestamps.
+	TimestampFormat string
+}
+
+func levelToString(level logrus.Level) string {
+	switch level {
+	case logrus.DebugLevel:
+		return "debug"
+	case logrus.InfoLevel:
+		return "info"
+	case logrus.WarnLevel:
+		return "warning"
+	case logrus.ErrorLevel:
+		return "error"
+	case logrus.FatalLevel:
+		return "critical"
+	case logrus.PanicLevel:
+		return "critical"
+	}
+
+	return "info"
+}
+
+func (f *LogrusGoogleCloudFormatter) Format(entry *logrus.Entry) ([]byte, error) {
+	fields := make(logrus.Fields)
+
+	for k, v := range entry.Data {
+		fields[k] = v
+	}
+
+	fields["time"] = entry.Time.Format(time.RFC3339Nano)
+	fields["message"] = entry.Message
+	fields["severity"] = levelToString(entry.Level)
+
+	serialized, err := json.Marshal(fields)
+	if err != nil {
+		return nil, fmt.Errorf("Failed to marshal fields to JSON, %v", err)
+	}
+	return append(serialized, '\n'), nil
+}
